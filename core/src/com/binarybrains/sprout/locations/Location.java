a4e4ef4159ac8255b2297e09befdca39b7fdf318
elundin <erik.lundin@ea.com>
2017-02-21 14:59:09 +0100

    Added Location class. Will hold info on geographic locations in the game.

---
+++ core/src/com/binarybrains/sprout/locations/Location.java
@@ -0,0 +1,12 @@
+package com.binarybrains.sprout.locations;
+
+public class Location {
+
+    private String name, description;
+    private int posX, posY; // rect?
+
+    public Location(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+}
