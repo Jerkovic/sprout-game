9f3f5c533e40ebf55cee5e3f3c02c1fbd62658f6
erikl <erilundin@gmail.com>
2017-02-26 22:45:04 +0100

    Added attachable interface

---
+++ core/src/com/binarybrains/sprout/item/Attachable.java
@@ -0,0 +1,10 @@
+package com.binarybrains.sprout.item;
+
+/**
+ * Created by erikl on 26/02/17.
+ * Example
+ * -Bates for FishingPole
+ * -Water for Watering Can
+ */
+public interface Attachable {
+}
--- core/src/com/binarybrains/sprout/item/tool/FishingPole.java
+++ core/src/com/binarybrains/sprout/item/tool/FishingPole.java
@@ -1,12 +1,15 @@
 package com.binarybrains.sprout.item.tool;
 
 
-public class FishingPole extends Tool {
-    public int bate = 5; // should this be some sort of smart attachment?
+import com.binarybrains.sprout.item.Attachable;
+
+public class FishingPole extends Tool implements Attachable {
+    public int bate = 50000; // should this be some sort of smart attachment?
     public FishingPole() {
         super("Fishing Pole", "A long, tapering rod to which a fishing line is attached.");
     }
 
+    @Override
     public boolean use() {
         if (bate < 1) {
             return false;
