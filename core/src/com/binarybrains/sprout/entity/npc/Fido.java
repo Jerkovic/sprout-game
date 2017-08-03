8746962c4c8ac228a740f35664af31e8d514bbbb
elundin <erik.lundin@ea.com>
2017-08-03 14:55:03 +0200

    Added temporary bark sound.

--- core/src/com/binarybrains/sprout/entity/npc/Fido.java
+++ core/src/com/binarybrains/sprout/entity/npc/Fido.java
@@ -1,12 +1,15 @@
 package com.binarybrains.sprout.entity.npc;
 
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
+import com.binarybrains.sprout.SproutGame;
 import com.binarybrains.sprout.level.Level;
 
 public class Fido extends Npc {
 
     private float changeTimer = 0;
+    Sound testSfx;
 
     public Fido(Level level, Vector2 position) {
         super(level, position, 16f, 16f, 4); // 4 is the sprite row
@@ -14,6 +17,11 @@ public class Fido extends Npc {
         setState(State.WALKING);
         setActionState(ActionState.EMPTY_NORMAL);
         setSpeed(MathUtils.random(32f, 32f));
+
+        // temp dog sound
+        testSfx = SproutGame.assets.get("sfx/dog_woof.wav");
+
+
     }
 
     @Override
@@ -21,9 +29,10 @@ public class Fido extends Npc {
         super.update(delta);
         changeTimer = changeTimer + delta;
 
-        if (changeTimer > 1 && distanceTo(getLevel().player) < 100) {
+        if (changeTimer > 2 && distanceTo(getLevel().player) < 100) {
             lookAt(getLevel().player);
             changeTimer = 0;
+            testSfx.play();
         }
         if (getState() == State.STANDING) {
             setState(State.WALKING);
