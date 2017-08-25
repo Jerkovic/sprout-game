0c226b067f878f3960f1c4e33593bb1476f5935f
erikl <erilundin@gmail.com>
2017-08-25 21:15:25 +0200

    Misc updates

--- desktop/src/com/binarybrains/sprout/desktop/DesktopLauncher.java
+++ desktop/src/com/binarybrains/sprout/desktop/DesktopLauncher.java
@@ -1,5 +1,6 @@
 package com.binarybrains.sprout.desktop;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.binarybrains.sprout.SproutGame;
@@ -8,7 +9,7 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 
-        if (true == false) { // todo args instead and create two runners
+        if (true == true) { // todo args instead and create two runners
             config.width = 1440; //1440;
             config.height = 900;//900;
             config.fullscreen = true;
@@ -20,6 +21,8 @@ public class DesktopLauncher {
             config.fullscreen = false;
 
         }
+
+        // config.disableAudio = true;
         config.resizable = false;
         config.useGL30 = false;
 		config.title = SproutGame.name;
