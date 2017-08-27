18d762e9ee0592d84b2f9a065faf0a555760e153
erikl <erilundin@gmail.com>
2017-08-27 16:23:56 +0200

    Added some missing assets for farm house

---
+++ core/assets/levels/farmhouse.tsx
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="stardew_valley_farmhouse" tilewidth="16" tileheight="16" tilecount="240" columns="12">
+ <image source="stardew_valley_farmhouse.png" trans="ffffff" width="192" height="320"/>
+ <tile id="37">
+  <properties>
+   <property name="blocked" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="38">
+  <properties>
+   <property name="blocked" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="39">
+  <properties>
+   <property name="blocked" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="40">
+  <properties>
+   <property name="blocked" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="41">
+  <properties>
+   <property name="blocked" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="42">
+  <properties>
+   <property name="blocked" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="43">
+  <properties>
+   <property name="blocked" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="137" type="teleporter">
+  <properties>
+   <property name="tileType" value="teleporter"/>
+  </properties>
+ </tile>
+</tileset>
