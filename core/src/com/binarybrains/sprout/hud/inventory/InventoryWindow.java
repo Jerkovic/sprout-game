1e099b991f16b74a1fc503ee2f6d9d746c815047
erikl <erilundin@gmail.com>
2017-09-04 20:55:57 +0200

    Added right mouse button listener in Inventory.

--- core/src/com/binarybrains/sprout/hud/inventory/InventoryWindow.java
+++ core/src/com/binarybrains/sprout/hud/inventory/InventoryWindow.java
@@ -1,11 +1,14 @@
 package com.binarybrains.sprout.hud.inventory;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.Align;
 import com.binarybrains.sprout.SproutGame;
 import com.binarybrains.sprout.entity.Inventory;
@@ -139,12 +142,22 @@ public class InventoryWindow extends Window {
                 button.setChecked(true);
             }
 
+            button.addListener(new ClickListener(Input.Buttons.RIGHT)
+            {
+                @Override
+                public void clicked(InputEvent event, float x, float y)
+                {
+                    // System.out.println("right mouse button");
+                }
+            });
+
             button.addListener(new ChangeListener() {
                 @Override
                 public void changed(ChangeEvent event, Actor actor) {
                     if (group.getCheckedIndex() > -1) {
                         level.player.setActiveItem(inventory.getItems().get(group.getCheckedIndex()));
                     }
+
                 }
             });
 
