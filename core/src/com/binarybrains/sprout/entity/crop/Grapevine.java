44c78a3f45d1406de9c5a6cc1d1f480168b53bf1
elundin <erik.lundin@ea.com>
2018-04-19 10:32:30 +0200

    grapevine crop added

---
+++ core/src/com/binarybrains/sprout/entity/crop/Grapevine.java
@@ -0,0 +1,93 @@
+package com.binarybrains.sprout.entity.crop;
+
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Array;
+import com.binarybrains.sprout.SproutGame;
+import com.binarybrains.sprout.entity.Entity;
+import com.binarybrains.sprout.entity.Mob;
+import com.binarybrains.sprout.entity.PickupItem;
+import com.binarybrains.sprout.entity.Player;
+import com.binarybrains.sprout.item.Item;
+import com.binarybrains.sprout.item.ResourceItem;
+import com.binarybrains.sprout.item.ToolItem;
+import com.binarybrains.sprout.item.resource.Resources;
+import com.binarybrains.sprout.item.tool.WateringCan;
+import com.binarybrains.sprout.level.Level;
+import com.binarybrains.sprout.level.tile.DirtTile;
+import com.binarybrains.sprout.level.tile.Tile;
+
+
+public class Grapevine extends Entity {
+
+    private Array<TextureAtlas.AtlasRegion> regions;
+    private int regionIndex = 0;
+    private float growTimer = 0;
+    public boolean watered = false;
+
+    public Grapevine(Level level, int tx, int ty) {
+
+        super(level, new Vector2(16f * tx, 16f * ty+4), 16, 16);
+        TextureAtlas atlas = SproutGame.assets.get("items2.txt");
+        regions = atlas.findRegions("Potato_Stage"); // potato test
+
+    }
+
+    @Override
+    public boolean interact(Player player, Item item, Mob.Direction attackDir) {
+        if (item instanceof ToolItem) {
+            ToolItem toolItem = (ToolItem) item;
+            if (toolItem.tool instanceof WateringCan && toolItem.tool.canUse()) {
+                ((WateringCan)toolItem.tool).pour();
+                player.getLevel().screen.hud.refreshInventory();
+                watered = true;
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    @Override
+    public void draw(Batch batch, float parentAlpha) {
+        batch.draw(regions.get(regionIndex), getX(), getY(), 16, 16);
+    }
+
+    public boolean canHarvest() {
+        return (regionIndex == 5);
+    }
+
+    @Override
+    public void update(float deltaTime) {
+        super.update(deltaTime);
+
+        // if not watered ... it is not growing
+        if (!watered) return;
+
+        growTimer = growTimer + deltaTime;
+        if (growTimer > 2 && regionIndex < 5) {
+            regionIndex++;
+            growTimer = 0;
+        }
+
+        // can harvest and the time is right we make potatoes jump out
+        if (growTimer > 5 && growTimer < 6 && canHarvest()) {
+            //System.out.println(this +" can be harvested!");
+            remove();
+            // set back the tile to Dirt instead of FarmTile
+            Tile tile = getLevel().getTile(getTileX(), getTileY());
+            if (tile instanceof DirtTile) {
+                ((DirtTile) tile).hasCrops = false;
+            }
+
+            int count = MathUtils.random(2, 6);
+            for (int i = 0; i < count; i++) {
+                getLevel().add(getLevel(), new PickupItem(getLevel(), new ResourceItem(Resources.potato), new Vector2(getPosition().x, getPosition().y)));
+            }
+
+        }
+
+    }
+}
