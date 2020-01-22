a32dda419d14b991758c44e79053c53cf60ce64f
Jerkovic <erilundin@gmail.com>
2020-01-22 19:25:48 +0100

    More sprite stuff drawn

--- desktop/src/com/binarybrains/sprout/desktop/DesktopLauncher.java
+++ desktop/src/com/binarybrains/sprout/desktop/DesktopLauncher.java
@@ -6,8 +6,6 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.binarybrains.sprout.SproutGame;
 import com.binarybrains.sprout.util.SheetGenerator;
 
-import java.awt.*;
-
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
@@ -21,15 +19,14 @@ public class DesktopLauncher {
         System.out.println("============================================================");
         Graphics.DisplayMode displayMode = LwjglApplicationConfiguration.getDesktopDisplayMode();
         config.setFromDisplayMode(displayMode);
-        config.width = 1920;
+        config.width = 1900;
         config.height = 1200;
-        config.fullscreen = false;
+        config.fullscreen =  true;
         config.resizable = false;
         config.useGL30 = false;
 		config.title = SproutGame.name;
         config.vSyncEnabled = true;
-        config.forceExit = false;
-
+        config.forceExit = true;
         // Window icons: 128x128(Mac), 32x32 (for Win and Linux), and 16x16 (for Win).
         // config.addIcon("some icon", Files.FileType.Local);
         new LwjglApplication(new SproutGame(), config);
